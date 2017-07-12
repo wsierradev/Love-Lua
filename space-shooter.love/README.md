afabe2226ec552a927b7b2fe08eec3b3f7f92425
servbotsu <tealsasuke@aol.com>
2017-07-12 11:40:10 -0400

    added space shooter project

---
+++ space-shooter.love/README.md
@@ -0,0 +1,17 @@
+# Space Shooter
+
+A simple "Space Invaders" style shooter game made with the LÖVE(love2d) framework utilizing the Lua language
+
+## To run program
+
+* Install love2d by visiting https://love2d.org/
+
+* Run space-shooter.love in your terminal or drag file over the love2d executable
+
+##Controls
+
+* Left/Right button on the keyboard to move your ship
+
+* Space button to shoot
+
+* Left Shift button to speed up movement
---
+++ space-shooter.love/main.lua
@@ -0,0 +1,92 @@
+love.graphics.setDefaultFilter('nearest', 'nearest')
+
+enemy = {}
+enemies_controller = {}
+enemies_controller.enemies = {}
+enemies_controller.image = love.graphics.newImage('enemy.png')
+
+
+function love.load()
+  player = {}
+  player.image = love.graphics.newImage('player.png')
+  player.x = 0
+  player.y = 560
+  player.bullets = {}
+  player.cooldown = 10
+  player.fire = function()
+    if player.cooldown <= 0 then
+      player.cooldown = 10
+      bullet = {}
+      bullet.x = player.x + 10
+      bullet.y = player.y
+      table.insert(player.bullets, bullet)
+    end
+  end
+    enemies_controller:spawnEnemy(0, 0)
+    enemies_controller:spawnEnemy(100, 0)
+end
+
+function enemies_controller:spawnEnemy(x, y)
+  enemy = {}
+  enemy.x = x
+  enemy.y = y
+  enemy.bullets = {}
+  enemy.cooldown = 10
+  table.insert(self.enemies, enemy)
+end
+
+function enemy:fire()
+  if self.cooldown <= 0 then
+    self.cooldown = 10
+    bullet = {}
+    bullet.x = self.x + 45
+    bullet.y = self.y
+    table.insert(self.bullets, bullet)
+  end
+end
+
+
+function love.update(dt)
+  player.cooldown = player.cooldown - 1
+  runspeed = 2
+  if love.keyboard.isDown("lshift") then
+    runspeed = 4
+  end
+  if love.keyboard.isDown("right") then
+    player.x = player.x + runspeed
+  elseif love.keyboard.isDown("left") then
+    player.x = player.x - runspeed
+  end
+
+  if love.keyboard.isDown("space") then
+    player.fire()
+  end
+
+  for _,e in pairs(enemies_controller.enemies) do
+    e.y = e.y + 1
+  end
+
+  for i,b in ipairs(player.bullets) do
+    if b.y < -1 then
+      table.remove(player.bullets, i)
+    end
+    b.y = b.y - 10
+
+  end
+end
+
+function love.draw()
+  --player
+  love.graphics.setColor(255,192,203)
+  love.graphics.draw(player.image, player.x, player.y, 0, 1)
+  --enemies
+  love.graphics.setColor(255,255,255)
+  for _,e in pairs(enemies_controller.enemies) do
+    love.graphics.draw(enemies_controller.image, e.x, e.y, 0, 2)
+  end
+  --bullets
+  love.graphics.setColor(0,0,255)
+  for _,b in pairs(player.bullets) do
+    love.graphics.rectangle("fill", b.x, b.y, 10, 10)
+  end
+end
